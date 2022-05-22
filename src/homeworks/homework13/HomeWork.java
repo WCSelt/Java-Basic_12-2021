493dbc7d351c611fdb598c4f09a36c4b2d8d590f
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-23 00:14:21 +0300

    add project to package homework13

--- src/homeworks/homework13/HomeWork.java
+++ src/homeworks/homework13/HomeWork.java
@@ -1,8 +1,15 @@
 package homeworks.homework13;
 
-
-public class HomeWork {
+class HomeWork {
     public static void main(String[] args) {
-
+        try {
+            Test test = new Test();
+            System.out.println("Приступаем к тестированию.");
+            test.beginTest();
+            System.out.println("Тестирование завершено.");
+            System.out.println(test.getQuestionnaire().getResult());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
