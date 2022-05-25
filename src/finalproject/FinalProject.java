3bb984cd6eb3aeb47f63613c6aad3625707b107a
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-25 09:23:00 +0300

    edit FinalProject

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -3,8 +3,8 @@ package finalproject;
 import finalproject.app.PrescriptionPrice;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Currency;
 
 public class FinalProject {
     public static void main(String[] args) {
@@ -12,6 +12,7 @@ public class FinalProject {
         try {
             PrescriptionPrice prescriptionPrice = new PrescriptionPrice(reader.readLine());
             System.out.println(prescriptionPrice.getNumberToString());
+            System.out.println();
         } catch (NullPointerException e) {
             System.out.println("Пожалуйста, проверьте ввод");
         } catch (Exception e) {
