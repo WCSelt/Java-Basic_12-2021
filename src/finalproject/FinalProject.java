41c7104c4172ff25129120c7d4594eef4321c3dd
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-26 17:08:46 +0300

    edit FinalProject

--- src/finalproject/FinalProject.java
+++ src/finalproject/FinalProject.java
@@ -2,13 +2,14 @@ package finalproject;
 
 import finalproject.app.CustomerRequest;
 import finalproject.app.CustomerRequestImpl;
-
+import finalproject.tests.AllTests;
 
 public class FinalProject {
     public static void main(String[] args) {
         try {
             CustomerRequest customerRequest = new CustomerRequestImpl();
             customerRequest.getResult();
+            new AllTests().runTests();
         } catch (NullPointerException e) {
             System.err.println("Кажется что-то пошло не так"+e);
         } catch (Exception e) {
