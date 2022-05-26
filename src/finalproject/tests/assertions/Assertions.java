8ca99eb0431cd308f91b6fa8f9ef8e388f18547c
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-26 16:51:36 +0300

    edit Assertions

--- src/finalproject/tests/assertions/Assertions.java
+++ src/finalproject/tests/assertions/Assertions.java
@@ -13,25 +13,9 @@ public class Assertions {
         }
     }
 
-    public static void assertEquals(Class<? extends Throwable> expected, Class<? extends Throwable> actual) {
-        if (!expected.equals(actual)) {
+    public static void assertContains(String expected, String actual) {
+        if (!actual.contains(expected)) {
             throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected, actual));
-        }
-    }
-
-    public static void assertThrows(Class<? extends Throwable> expected, Runnable code) {
-        Throwable actual = null;
-        try {
-            code.run();
-        } catch (Throwable e) {
-            actual = e;
-        }
-        if (actual == null) {
-            throw new AssertionError("Given code does not throw any exception");
-        }
-
-        if (!actual.getClass().equals(expected)) {
-            throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected.getSimpleName(), actual.getClass().getSimpleName()));
         }
     }
 }
