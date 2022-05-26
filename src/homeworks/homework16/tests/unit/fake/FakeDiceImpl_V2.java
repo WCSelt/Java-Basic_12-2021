2eca421cd60f006bcbc45ebb5b4cf5127fffd022
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-26 13:41:13 +0300

    clean FakeDiceImpl_V2

--- src/homeworks/homework16/tests/unit/fake/FakeDiceImpl_V2.java
+++ src/homeworks/homework16/tests/unit/fake/FakeDiceImpl_V2.java
@@ -5,12 +5,7 @@ import homeworks.homework16.app.game.Dice;
 public class FakeDiceImpl_V2 implements Dice {
     boolean switcher;
     public int roll() {
-        if (!switcher){
-            switcher = !switcher;
-            return 4;
-        } else {
-            switcher = !switcher;
-            return 3;
-        }
+        switcher = !switcher;
+        return switcher ? 4 : 3;
     }
 }
