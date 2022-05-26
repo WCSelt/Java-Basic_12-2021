43f3d8b0885e0e519f6a763324a20279fe7fcc02
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-26 14:20:31 +0300

    add classes AllTests, PrescriptionPriceTest, Assertions

---
+++ src/finalproject/tests/AllTests.java
@@ -0,0 +1,7 @@
+package finalproject.tests;
+
+public class AllTests {
+    public void runTests () {
+
+    }
+}
---
+++ src/finalproject/tests/unit/PrescriptionPriceTest.java
@@ -0,0 +1,22 @@
+package finalproject.tests.unit;
+
+import finalproject.app.*;
+import finalproject.tests.assertions.Assertions;
+import org.junit.Test;
+
+public class PrescriptionPriceTest {
+    @Test
+    public void firstTest() {
+        String scenario;
+    }
+
+    @Test
+    public void secondTest() {
+        String scenario;
+    }
+
+    @Test
+    public void thirdTest() {
+        String scenario;
+    }
+}
