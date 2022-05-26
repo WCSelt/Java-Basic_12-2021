f8a1c767bdad6fed0153d7babdb5d848c57f783b
AlexanderZakharov <zakharov1992a@gmail.com>
2022-05-26 13:25:26 +0300

    edit new assertion

--- src/homeworks/homework16/tests/assertions/Assertions.java
+++ src/homeworks/homework16/tests/assertions/Assertions.java
@@ -10,7 +10,7 @@ public class Assertions {
     }
 
     public static void assertEquals(Player expected, Player actual) {
-        if (!expected.equals(actual)) {
+        if (expected.hashCode()!= actual.hashCode() & !expected.equals(actual)) {
             throw new AssertionError(String.format("Expected \"%s\" = \"%s\"", expected, actual));
         }
     }
